510b70f58ec61d08329b8d2ea6de84089a4418eb
Abhishek <abhishekshrm2001@gmail.com>
2023-12-23 19:23:48 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
   return (
     <>
       <h1>HELLO REDUX</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
--- src/components/Todo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Todo() {
-  return (
-    <div>Todo</div>
-  )
-}
-
-export default Todo
